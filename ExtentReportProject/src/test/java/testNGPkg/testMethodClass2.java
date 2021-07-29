0e9067aa1fe4fcf63f06e365846bfa3609a3125c
Lakshmi <tslmadhuri@gmail.com>
2021-07-28 22:00:45 -0400

    test commit

--- ExtentReportProject/src/test/java/testNGPkg/testMethodClass2.java
+++ ExtentReportProject/src/test/java/testNGPkg/testMethodClass2.java
@@ -9,7 +9,7 @@ public class testMethodClass2 {
 	
 	@Test
 	public void testMethod4() {
-		System.out.println("I am in test method4_test");
+		System.out.println("I am in test method4_test1");
 	}
 	
 	@Test
